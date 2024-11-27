55c8ed06b1e461d3517cf9a6bb1bc4202a3e3a94
noseunghyeon <ro981009@gmail.com>
2024-11-27 14:47:14 +0900

    ro

--- front/src/components/Search.jsx
+++ front/src/components/Search.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Map from "./map/Map";
 
 const search = () => {
-  return <div style={{ paddingTop: "4rem" }}>search</div>;
+  return (
+    <div style={{ paddingTop: "4rem" }}>
+      <Map />
+    </div>
+  );
 };
 
 export default search;
