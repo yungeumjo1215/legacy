d80b8a3b223510f06bc0c1debe410d95e8a37037
jiwon010106 <jjiwon753@gmail.com>
2024-11-28 16:30:21 +0900

    Slices

--- front/src/redux/slices/apiSlices.js
+++ front/src/redux/slices/apiSlices.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getRequest } from "../../utils/requestMethods";
+
+import { GET_HERITAGE_API_URL } from "../../utils/apiUrl";
 
 const getHeritageFetchThunk = (actionType, apiURL) => {
   return createAsyncThunk(actionType, async (userId) => {
@@ -15,9 +18,20 @@ export const fetchGetHeritageData = getHeritageFetchThunk(
   GET_HERITAGE_API_URL // 요청 url
 ); // thunk 함수 호출
 
+// handleFulfilled 함수 정의 : 요청 성공 시 상태 업데이트 로직을 별도의 함수로 분리
+const handleFulfilled = (stateKey) => (state, action) => {
+  state[stateKey] = action.payload; // action.payload에 응답 데이터가 들어있음
+};
+
+// handleRejected 함수 정의 : 요청 실패 시 상태 업데이트 로직을 별도의 함수로 분리
+const handleRejected = (state, action) => {
+  console.log("Error", action.payload);
+  state.isError = true;
+};
+
 const apiSlices = createSlice({
   name: "apis",
-  getHeritageData: null,
+  initialState: { getHeritageData: null },
 
   extraReducers: (builder) => {
     builder
