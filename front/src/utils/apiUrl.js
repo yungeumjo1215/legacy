010241e0ea57593c0a9efbb7e9ffca269676c4d9
rommaniitedomum <qkrwns982@gmail.com>
2024-11-29 16:20:11 +0900

    api updated

--- front/src/utils/apiUrl.js
+++ front/src/utils/apiUrl.js
@@ -4,10 +4,14 @@ const GET_HERITAGE_API_URL = `${rootPath}/heritage`;
 const GET_FESTIVAL_API_URL = `${rootPath}/festival`;
 const CREATE_ACCOUNT_API_URL = `${rootPath}/account/create`;
 const DELETE_ACCOUNT_API_URL = `${rootPath}/account/delete/:uuid`;
+const LOGIN_ACCOUNT_API_URL = `${rootPath}/account/login`;
+const LOGOUT_ACCOUNT_API_URL = `${rootPath}/account/logout`;
 
 export {
   GET_HERITAGE_API_URL,
   CREATE_ACCOUNT_API_URL,
   DELETE_ACCOUNT_API_URL,
   GET_FESTIVAL_API_URL,
+  LOGOUT_ACCOUNT_API_URL,
+  LOGIN_ACCOUNT_API_URL,
 };
