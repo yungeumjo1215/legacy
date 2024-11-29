9e20ddda29790ad3899aa24c13c8b0fb1931b572
Seoyoung-Cheon <gu05087@gmail.com>
2024-11-29 13:15:41 +0900

    Modal44

--- front/src/components/Modal.jsx
+++ front/src/components/Modal.jsx
@@ -1,3 +1,4 @@
+// Modal.jsx
 import React from "react";
 
 const Modal = ({ item, onClose }) => {
@@ -22,31 +23,52 @@ const Modal = ({ item, onClose }) => {
           transform: "translate(-50%, -50%)",
           backgroundColor: "#fff",
           color: "black",
-          padding: "20px",
+          padding: "30px", // padding을 늘려서 여백을 좀 더 추가
           borderRadius: "8px",
           zIndex: 10000,
-          width: "600px",
-          height: "400px",
+          width: "800px", // 모달 너비를 800px로 늘림
+          height: "800px", // 모달 높이를 600px로 늘림
           overflowY: "auto",
         }}
         onClick={(e) => e.stopPropagation()} // 모달 내부 클릭 시 이벤트 버블링 방지
       >
-        <h2 style={{ fontSize: "24px", marginBottom: "20px" }}>
+        <h2
+          style={{ fontSize: "28px", marginBottom: "20px", fontWeight: "bold" }}
+        >
           {item.ccbaMnm1} {/* 유적지 이름 */}
         </h2>
         <img
           src={item.imageUrl} // 유적지 이미지 URL
           alt={item.ccbaMnm1}
-          style={{ width: "100%", borderRadius: "8px", marginTop: "20px" }}
+          style={{
+            width: "100%",
+            borderRadius: "8px",
+            marginBottom: "20px", // 이미지 아래 여백 추가
+            maxHeight: "350px", // 이미지 최대 높이를 350px로 제한
+            objectFit: "cover", // 이미지가 잘리거나 왜곡되지 않도록
+          }}
         />
-        <p>
-          <strong>설명:</strong> {item.content} {/* 유적지 설명 */}
+        <p
+          style={{
+            fontSize: "18px",
+            marginBottom: "20px",
+            boxSizing: "border-box",
+            border: "1px solid gray",
+            padding: "10px",
+          }}
+        >
+          {item.content} {/* 유적지 설명 */}
         </p>
-        <p>
+        <p
+          style={{
+            fontSize: "16px",
+            marginBottom: "10px",
+          }}
+        >
           <strong>위치:</strong> {item.ccbaLcad} {/* 유적지 위치 */}
         </p>
-        <p>
-          <strong>유적지 이름:</strong> {item.ccceName} {/* 유적지 이름 */}
+        <p style={{ fontSize: "16px", marginBottom: "20px" }}>
+          <strong>시대:</strong> {item.ccceName} {/* 유적지 이름 */}
         </p>
 
         <button
@@ -55,10 +77,14 @@ const Modal = ({ item, onClose }) => {
             backgroundColor: "#121a35",
             color: "white",
             border: "none",
-            padding: "10px 20px",
+            padding: "12px 25px", // 버튼 크기 키움
             borderRadius: "5px",
             cursor: "pointer",
             marginTop: "20px",
+            display: "flex", // flexbox를 사용하여 정렬
+            justifyContent: "center", // 가로로 가운데 정렬
+            width: "100%", // 버튼의 너비를 100%로 설정하여 부모 요소 안에서 가로로 가운데 배치
+            alignItems: "center", // 버튼 내용 가로 세로 모두 가운데로 정렬
           }}
         >
           닫기
