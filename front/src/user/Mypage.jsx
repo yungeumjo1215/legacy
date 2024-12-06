d9f4dae8071bbf83da04a145f09af536b5065f6e
song426 <seolsongie@gmail.com>
2024-12-06 15:53:17 +0900

    mypage

--- front/src/user/Mypage.jsx
+++ front/src/user/Mypage.jsx
@@ -71,8 +71,8 @@ const Mypage = () => {
   }
 
   return (
-    <div className="h-[calc(100vh-64px)] w-screen bg-gradient-to-tr from-blue-100 via-white to-white mt-16">
-      <div className="flex  w-full p-4 gap-4">
+    <div className="min-h-[calc(100vh-64px)] w-full bg-gradient-to-tr from-blue-100 via-white to-white mt-16">
+      <div className="flex w-full p-4 gap-4">
         {/* 사이드바 */}
         <div
           className={`w-80 bg-white/80 backdrop-blur-sm rounded-2xl shadow-lg border border-gray-100 p-8 transition-all duration-300 hover:shadow-xl ${
