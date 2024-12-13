3e123cbd398a15ed3316379621d9465f676afd90
yungeumjo1215 <elec0807@gmail.com>
2024-12-13 10:17:48 +0900

    chat

--- front/src/components/chatbot/config.js
+++ front/src/components/chatbot/config.js
@@ -3,6 +3,11 @@ import { createChatBotMessage } from "react-chatbot-kit";
 const config = {
   initialMessages: [createChatBotMessage("안녕하세요! 무엇을 도와드릴까요?")],
   botName: "Chatbot",
+  customComponents: {
+    header: () => (
+      <div className="react-chatbot-kit-chat-header">유산지기 AI 챗봇</div>
+    ),
+  },
   customStyles: {
     botMessageBox: {
       backgroundColor: "#376B7E",
