5a664d630bfb3704bbff54596a06941d60e181e9
yungeumjo1215 <elec0807@gmail.com>
2024-12-13 10:02:52 +0900

    chat

--- front/src/components/chatbot/Chatbot.jsx
+++ front/src/components/chatbot/Chatbot.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
+import "./Chatbot.css";
 import config from "./config";
 import MessageParser from "./MessageParser";
 import ActionProvider from "./ActionProvider";
