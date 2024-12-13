fb2657c3ee09702220fc69118f7b2bab78f42a83
noseunghyeon <ro981009@gmail.com>
2024-12-13 09:23:50 +0900

    chatbot

--- front/src/components/chatbot/Chatbot.jsx
+++ front/src/components/chatbot/Chatbot.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
-import config from "../chatbot/config";
-import MessageParser from "../chatbot/MessageParser";
-import ActionProvider from "../chatbot/ActionProvider";
+import config from "./config";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
 
 const Chat = () => {
   return (
-    <div style={{ maxWidth: "100%", margin: "0 auto" }}>
-      <Chatbot
-        config={config}
-        messageParser={MessageParser}
-        actionProvider={ActionProvider}
-        placeholderText="메시지를 입력하세요..."
-      />
+    <div className="chat-wrapper">
+      <div className="chat-container">
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+          placeholderText="메시지를 입력하세요..."
+        />
+      </div>
     </div>
   );
 };
