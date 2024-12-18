bfe6b643f70292acb2d5e816f8f19dac860397b1
noseunghyeon <ro981009@gmail.com>
2024-12-18 16:33:00 +0900

    clear

--- front/src/components/chatbot/MessageParser.js
+++ front/src/components/chatbot/MessageParser.js
@@ -6,6 +6,7 @@ class MessageParser {
 
   parse(message) {
     if (!message) return;
+    console.log("사용자 메시지:", message); // 디버깅용
 
     // 일반 질문 처리
     this.actionProvider.handleMessage(message);
